6143831298e055a2ab74be4d47efbd0c7548c90e
lrz <1051810696@qq.com>
2022-06-25 17:52:05 +0800

    组件

---
+++ 19_单文件组件/App.vue
@@ -0,0 +1,21 @@
+<template>
+<!--    模板里面必须有一个根元素-->
+    <div>
+        <School></School>
+        <Student></Student>
+    </div>
+</template>
+
+<script>
+    // 引入组件
+    import School from "./School";
+    import Student from "./Student";
+
+    export default {
+        name:"App",
+        components:{
+            School,
+            Student,
+        }
+    }
+</script>
